5c55bbfd318e4f24900287a9bbe8635a12edbdba
Shevchenko Timofei <tima9953@gmail.com>
2021-07-09 16:49:36 +0600

    created task1dop.sh

---
+++ task1dop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ mkdir projects
+ cd projects 
+ mkdir myproject
+ cd myproject 
+mkdir directory1 directory2 directory3
+cd directory1
+ mkdir subdirectory1
+ cd ../
+ cd directory3
+ mkdir subdirectory3
+cd ../..
+projects % 
